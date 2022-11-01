c9bd2a6ddf8871f2bbfce6f93a4cdb97f3274287
Marcelo Heinrick <marcelohz198@hotmail.com>
2022-11-01 11:59:38 -0300

    Folder Path Error

--- functions/config.js
+++ functions/config.js
@@ -85,7 +85,7 @@ module.exports = {
             folderVerify(['css', 'js'], pathToCreate + '/public')
     
             fs.copyFile(__dirname + '/../jquery-atual.js',  pathToCreate + '/public/js/jquery-atual.js', err => console.log('') );
-            fs.copyFile(__dirname + '/../includes.html',  pathToCreate + '/includes.html', err => console.log('') );
+            fs.copyFile(__dirname + '/../includes.html',  pathToCreate + '/public/includes.html', err => console.log('') );
 
             console.log("\n**************************".yellow);
             console.log("!Atenção!".yellow.bold);
